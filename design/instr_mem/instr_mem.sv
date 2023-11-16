e8e6cec9135be0139a93858eee227a9b7ba8c30c
Sanjit Raman <ssr321@ic.ac.uk>
2023-11-16 11:50:23 +0000

    added instruction memory interface

---
+++ design/instr_mem/instr_mem.sv
@@ -0,0 +1,10 @@
+module instr_mem #(
+    parameter DATA_WIDTH = 32,
+    parameter ADDRESS_WIDTH = 2
+) (
+   input logic clk,
+   input logic [ADDRESS_WIDTH-1:0] a,
+   output logic [DATA_WIDTH-1:0] rd
+);
+    
+endmodule
