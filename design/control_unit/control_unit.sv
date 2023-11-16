e4c6e1e56e702de69b64c1ff5b18ba63f1b27596
Sanjit Raman <ssr321@ic.ac.uk>
2023-11-16 10:30:16 +0000

    updated to match diagram naming convention

--- design/control_unit/control_unit.sv
+++ design/control_unit/control_unit.sv
@@ -3,15 +3,13 @@ module control_unit #(
     parameter ALU_CTRL_WIDTH = 3,
 ) (
     input logic [DATA_WIDTH-1:0]      instr,
-    input logic                       eq,
+    input logic                       EQ,
     
-    output logic                      pc_src, 
-    output logic                      result_src,
-    output logic                      mem_write,
-    output logic [ALU_CTRL_WIDTH-1:0] alu_ctrl,
-    output logic                      alu_src,
-    output logic [1:0]                imm_src,
-    output logic                      reg_write
+    output logic                      PCsrc, 
+    output logic [ALU_CTRL_WIDTH-1:0] ALUctrl,
+    output logic                      ALUsrc,
+    output logic [1:0]                ImmSrc,
+    output logic                      RegWrite
 );
     
 endmodule
