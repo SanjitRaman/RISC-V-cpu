ae3d04838baeeb29fa01bef30862d63d852da75a
as9322 <147554866+as9322@users.noreply.github.com>
2023-11-20 11:04:18 +0000

    Update risc_v_tb.cpp
    Testbench template

---
+++ testbench/risc_v/risc_v_tb.cpp
@@ -0,0 +1,32 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vriscv.h"
+#define MAX_SIM_CYC 100000
+
+int main(int argc, char **argv, char **env) {
+  int simcyc;     // simulation clock count
+  int tick;       // each clk cycle has two ticks for two edges
+  
+  Verilated::commandArgs(argc, argv);
+  // init top verilog instance
+  Vf1light * top = new Vf1light;
+  // init trace dump
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace (tfp, 99);
+  tfp->open ("riscv.vcd");
+ 
+  // initialize simulation inputs
+  top->clk = 1;
+  top->rst = 0;
+  
+  // run simulation for MAX_SIM_CYC clock cycles
+  for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
+    // dump variables into VCD file and toggle clock
+    for (tick=0; tick<2; tick++) {
+      tfp->dump (2*simcyc+tick);
+      top->clk = !top->clk;
+      top->eval ();
+    }
+  }
+}
