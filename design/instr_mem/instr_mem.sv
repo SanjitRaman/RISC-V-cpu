bd44b9fcd9ea7d56ac3e705a7aed6458dcd5e45f
as9322 <147554866+as9322@users.noreply.github.com>
2023-11-21 11:45:38 +0000

    Update instr_mem.sv

--- design/instr_mem/instr_mem.sv
+++ design/instr_mem/instr_mem.sv
@@ -7,4 +7,15 @@ module instr_mem #(
    output logic [DATA_WIDTH-1:0] RD
 );
     
+logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
+
+initial begin
+    $display("Loading rom.");
+    $readmemh("testCode.mem", rom_array);
+end;
+
+always_ff @(posedge clk) begin
+    RD <= rom_array[A];
+    end
+
 endmodule
