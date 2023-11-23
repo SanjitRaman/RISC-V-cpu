4c60cf6ff01e455261973d06fe94a266dbed88e9
Sanjit Raman <ssr321@ic.ac.uk>
2023-11-23 21:37:53 +0000

    addi, bne, lw tested

--- testbench/control_unit/control_unit_tb.cpp
+++ testbench/control_unit/control_unit_tb.cpp
@@ -52,6 +52,54 @@ TEST_F(ControlUnitTest, ADDI) {
   ASSERT_EQ(top->MemWrite, 0);
 }
 
+TEST_F(ControlUnitTest, BNE0) {
+  //clock_ticks(simcyc);
+  top->op = 0b1100011;
+  top->funct3 = 0b001;
+  top->funct7_5 = 0b0;
+  top->Zero = 0b0;
+  top->eval();
+  ASSERT_EQ(top->RegWrite, 0);
+  ASSERT_EQ(top->ImmSrc, 0b10);
+  ASSERT_EQ(top->ALUSrc, 0);
+  ASSERT_EQ(top->MemWrite, 0);
+  ASSERT_EQ(top->ResultSrc, 0);
+  ASSERT_EQ(top->PCSrc, 1);
+  ASSERT_EQ(top->ALUControl, 0b001);
+}
+
+TEST_F(ControlUnitTest, BNE1) {
+  //clock_ticks(simcyc);
+  top->op = 0b1100011;
+  top->funct3 = 0b001;
+  top->funct7_5 = 0b0;
+  top->Zero = 0b1;
+  top->eval();
+  ASSERT_EQ(top->RegWrite, 0);
+  ASSERT_EQ(top->ImmSrc, 0b10);
+  ASSERT_EQ(top->ALUSrc, 0);
+  ASSERT_EQ(top->MemWrite, 0);
+  ASSERT_EQ(top->ResultSrc, 0);
+  ASSERT_EQ(top->PCSrc, 0);
+  ASSERT_EQ(top->ALUControl, 0b001);
+}
+
+TEST_F(ControlUnitTest, LW) {
+  //clock_ticks(simcyc);
+  top->op = 0b0000011;
+  top->funct3 = 0b010;
+  top->funct7_5 = 0b0;
+  top->Zero = 0b0;
+  top->eval();
+  ASSERT_EQ(top->PCSrc, 0);
+  ASSERT_EQ(top->ALUControl, 0b000);
+  ASSERT_EQ(top->ALUSrc, 1);
+  ASSERT_EQ(top->ImmSrc, 0b00);
+  ASSERT_EQ(top->RegWrite, 1);
+  ASSERT_EQ(top->ResultSrc, 1);
+  ASSERT_EQ(top->MemWrite, 0);
+}
+
 int main(int argc, char **argv) {
   Verilated::commandArgs(argc, argv);
   testing::InitGoogleTest(&argc, argv);
