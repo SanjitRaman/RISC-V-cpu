66eb69203017b2d39268cff8abaac0f4daaf366d
sri-yeah <sriyesh62@gmail.com>
2023-11-26 19:59:43 +0000

    R type instructions implemented
    changes to ALU decoder and ALU with own ALUControl signal map
    
    (this branch will forever live on in my heart as "r-tyep")

--- design/alu/alu.sv
+++ design/alu/alu.sv
@@ -1,6 +1,6 @@
 module alu #(
     parameter DATA_WIDTH = 32,
-    parameter ALU_CTRL_WIDTH = 3
+    parameter ALU_CTRL_WIDTH = 4
 )(
     input  logic [DATA_WIDTH-1:0]     SrcA,
     input  logic [DATA_WIDTH-1:0]     SrcB,
@@ -9,20 +9,38 @@ module alu #(
     output logic                      Zero
 );
 
-// 000 - add
-// 001 - subtract
-// 101 - set less than
-// 011 - or
-// 010 - and
+// 0000 - add
+// 0001 - subtract
+// 0010 - shift left logical
+// 0011 - set less than
+// 0100 - set less than unsigned 
+// 0101 - xor 
+// 0110 - shift right logical 
+// 0111 - shift right illogical 
+// 1000 - or
+// 1001 - and
+
+logic signs = {SrcA[DATA_WIDTH-1], SrcB[DATA_WIDTH-1]} ;
 
 always_comb begin
     case(ALUControl)
-        3'b000:  ALUResult = SrcA + SrcB;
-        3'b001:  ALUResult = SrcA - SrcB;
-        3'b101:  ALUResult = (SrcA < SrcB) ? {DATA_WIDTH{1'b1}} : {DATA_WIDTH{1'b0}};
-        3'b011:  ALUResult = SrcA | SrcB;
-        3'b010:  ALUResult = SrcA & SrcB;
-        default: ALUResult = SrcA + SrcB;
+        4'b0000:  ALUResult = SrcA + SrcB;
+        4'b0001:  ALUResult = SrcA - SrcB;
+        4'b0010:  ALUResult = (SrcA<<SrcB);
+        4'b0011:  case{signs}:
+                    00: ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
+                    01: ALUResult = {DATA_WIDTH{1'b0}};
+                    10: ALUResult = {{DATA_WIDTH{1'b0}}, {1'b1}};
+                    11: ALUResult = (SrcA[DATA_WIDTH-2:0] < SrcB[DATA_WIDTH-2:0]) ? {{DATA_WIDTH{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
+                    default: ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
+                endcase
+        4'b0100:  ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
+        4'b0101:  ALUResult = SrcA ^ SrcB;
+        4'b0110:  ALUResult = (SrcA>>SrcB);
+        4'b0111:  ALUResult = {{SrcB{SrcA[DATA_WIDTH-1]}}, (SrcA>>SrcB)[32-SrcB]};
+        4'b1000:  ALUResult = SrcA | SrcB;
+        4'b1001:  ALUResult = SrcA & SrcB;
+        default:  ALUResult = SrcA + SrcB;
     endcase
     Zero = ({DATA_WIDTH{1'b0}} == (SrcA ^ SrcB)) ? 1'b1 : 1'b0;
 end
--- design/control_unit/decoders/alu_decoder.sv
+++ design/control_unit/decoders/alu_decoder.sv
@@ -2,7 +2,7 @@ module alu_decoder #(
     parameter                         OP_WIDTH       = 7,
     parameter                         FUNCT3_WIDTH   = 3,
     parameter                         ALU_OP_WIDTH   = 2,
-    parameter                         ALU_CTRL_WIDTH = 3
+    parameter                         ALU_CTRL_WIDTH = 4
 
 
 ) (
@@ -18,31 +18,44 @@ module alu_decoder #(
     
     assign op_5_funct_5_5 = {op[5], funct7_5};
 
-    // From Table in Lecture 7 Slide 19
     always_comb
         case (ALUOp)
             2'b00:                                      // Load/Store (lw/sw)
-                ALUControl = 3'b000;                    //      add
+                ALUControl = 4'b0000;                    //      add
             2'b01:                                      // Branch (beq)
-                ALUControl = 3'b001;                    //      subtract
+                ALUControl = 4'b0001;                    //      subtract
             2'b10:                                      // R-Type
+
                 case (funct3)
                     3'b000:
                         if (op_5_funct_5_5 == 2'b11)
-                            ALUControl = 3'b001;        // subtract
+                            ALUControl = 4'b0001;           // subtract
                         else
-                            ALUControl = 3'b000;        // add
+                            ALUControl = 4'b0000;           // add
+                    3'b001:
+                            ALUControl = 4'b0010;           // sll 
                     3'b010:
-                        ALUControl = 3'b010;            // slt
+                        ALUControl = 4'b0011;               // slt
+                    3'b011:
+                        ALUControl = 4'b0100;               // sltu
+                    3'b100:
+                        ALUControl = 4'b0101;               // xor
+                    3'b101:
+                        if (op_5_funct_5_5 == 2'b10)
+                            ALUControl = 4'b0110;            // srl
+                        else if (op_5_funct_5_5 == 2'b11)
+                            ALUControl = 4'b0111;            // sra
+
                     3'b110:
-                        ALUControl = 3'b011;            // or
+                        ALUControl = 4'b011;                // or
                     3'b111:
-                        ALUControl = 3'b010;            // and
+                        ALUControl = 4'b010;                // and
                     default:
-                        ALUControl = 3'b000;            // add
+                        ALUControl = 4'b000;                // add
                 endcase
+
             default:
-                ALUControl = 3'b000;                    // add
+                ALUControl = 4'b000;                        // add
 
         endcase
     
