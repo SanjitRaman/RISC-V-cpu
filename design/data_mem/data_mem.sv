02ff0f597ca1b73be62ad25f7201ad708ebb7a04
as9322 <147554866+as9322@users.noreply.github.com>
2023-11-29 22:03:51 +0000

    Update data_mem.sv

--- design/data_mem/data_mem.sv
+++ design/data_mem/data_mem.sv
@@ -8,6 +8,7 @@ module data_mem #(
     input  logic                     WE,
     input  logic [ADDRESS_WIDTH-1:0] A,
     input  logic [DATA_WIDTH-1:0]    WD,
+    input  logic [2:0]               funct3,
     output logic [DATA_WIDTH-1:0]    RD
 );
 
@@ -25,5 +26,33 @@ always_ff @(posedge CLK)
         ram_array[A+2] <= WD[3*BYTE_WIDTH-1:2*BYTE_WIDTH];
         ram_array[A+3] <= WD[4*BYTE_WIDTH-1:3*BYTE_WIDTH]; // this will be a problem for sw: individual bytes are not written to.
     end
-assign RD = {{24{1'b0}}, ram_array[A]};
+
+always_comb begin
+        case (funct3)
+            3'b000: // Load byte (lb)
+                begin
+                    RD = {{24{1'bA[31]}}, ram_array[A]};
+                end
+            3'b001: // Load Half (lh)
+                begin
+                    RD = {{16{1'bA[31]}}, ram_array[A+1], ram_array[A]};
+                end
+            3'b010: // Load word (lw) 
+                begin
+                    RD = {ram_array[A+3], ram_array[A+2], ram_array[A+1], ram_array[A]};
+                end
+            3'b100: // Load byte unsigned (lbu)
+                begin
+                    RD = {{24{1'b0}}, ram_array[A]};
+                end
+            3'b101: // Load half unsigned (lhu)
+                begin
+                    RD = {{16{1'b0}}, ram_array[A+1], ram_array[A]};
+                end
+            default:
+                begin
+                    RD = {32{1'b0}};
+                end
+        endcase
+    end
 endmodule
