e50fa440d0ff0bc1d208ef48c1350dafda0bba6f
as9322 <147554866+as9322@users.noreply.github.com>
2023-11-29 22:39:30 +0000

    Update alu_tb.cpp

--- testbench/alu/alu_tb.cpp
+++ testbench/alu/alu_tb.cpp
@@ -22,7 +22,7 @@ protected:
 
 
 TEST_F(ALUTest, ADDZ0) {
-  top->ALUControl = 0b000;
+  top->ALUControl = 0b0000;
   top->SrcA = 0x0000003F;
   top->SrcB = 0x0000014A;
   top->eval();
@@ -31,7 +31,7 @@ TEST_F(ALUTest, ADDZ0) {
 }
 
 TEST_F(ALUTest, ADDZ1) {
-  top->ALUControl = 0b000;
+  top->ALUControl = 0b0000;
   top->SrcA = 0xFFFFFFFF;
   top->SrcB = 0x39E8F390;
   top->eval();
@@ -40,7 +40,7 @@ TEST_F(ALUTest, ADDZ1) {
 }
 
 TEST_F(ALUTest, SUBZ0) {
-  top->ALUControl = 0b001;
+  top->ALUControl = 0b0001;
   top->SrcA = 0x000037D8;
   top->SrcB = 0x0000025B;
   top->eval();
@@ -49,7 +49,7 @@ TEST_F(ALUTest, SUBZ0) {
 }
 
 TEST_F(ALUTest, SUBZ1) {
-  top->ALUControl = 0b001;
+  top->ALUControl = 0b0001;
   top->SrcA = 0x29AB0E38;
   top->SrcB = 0xFFFFF021;
   top->eval();
@@ -57,24 +57,112 @@ TEST_F(ALUTest, SUBZ1) {
   ASSERT_EQ(top->Zero, 0b1);
 }
 
-TEST_F(ALUTest, SLTG) {
-  top->ALUControl = 0b101;
+TEST_F(ALUTest, SLL) {
+  top->ALUControl = 0b0011;
+  top->SrcA = 0x000037D8;
+  top->SrcB = 3;
+  top->eval();
+  ASSERT_EQ(top->ALUResult, 0x00000000);
+}
+
+TEST_F(ALUTest, SLTGPP) {
+  top->ALUControl = 0b0011;
   top->SrcA = 0x000037D8;
   top->SrcB = 0x0000025B;
   top->eval();
   ASSERT_EQ(top->ALUResult, 0x00000000);
 }
 
-TEST_F(ALUTest, SLTL) {
-  top->ALUControl = 0b101;
+TEST_F(ALUTest, SLTGPN) {
+  top->ALUControl = 0b0011;
   top->SrcA = 0x29AB0E38;
   top->SrcB = 0xE010F021;
+  top->eval();
+  ASSERT_EQ(top->ALUResult, 0x00000000);
+}
+
+TEST_F(ALUTest, SLTGNN) {
+  top->ALUControl = 0b0011;
+  top->SrcA = 0xA0AB0E38;
+  top->SrcB = 0xF01FF021;
   top->eval();
   ASSERT_EQ(top->ALUResult, 0x11111111);
 }
 
+TEST_F(ALUTest, SLTLPP) {
+  top->ALUControl = 0b0011;
+  top->SrcA = 0x0000025B;
+  top->SrcB = 0x00007901;
+  top->eval();
+  ASSERT_EQ(top->ALUResult, 0x11111111);
+}
+
+TEST_F(ALUTest, SLTLNP) {
+  top->ALUControl = 0b0011;
+  top->SrcA = 0xF01FF021;
+  top->SrcB = 0x00AB0E38;
+  top->eval();
+  ASSERT_EQ(top->ALUResult, 0x11111111);
+}
+
+TEST_F(ALUTest, SLTLNN) {
+  top->ALUControl = 0b0011;
+  top->SrcA = 0xF01FF021;
+  top->SrcB = 0xA0AB0E38;
+  top->eval();
+  ASSERT_EQ(top->ALUResult, 0x11111111);
+}
+
+TEST_F(ALUTest, SLTU0) {
+  top->ALUControl = 0b0100;
+  top->SrcA = 0x735a1099;
+  top->SrcB = 0x30d2f191;
+  top->eval();
+  ASSERT_EQ(top->ALUResult, 0x00000000);
+}
+
+TEST_F(ALUTest, SLTU1) {
+  top->ALUControl = 0b0100;
+  top->SrcA = 0x30d2f191;
+  top->SrcB = 0x735a1099;
+  top->eval();
+  ASSERT_EQ(top->ALUResult, 0x11111111);
+}
+
+TEST_F(ALUTest, XOR) {
+  top->ALUControl = 0b0101;
+  top->SrcA = 0x6047D6D3;
+  top->SrcB = 0xD471D368;
+  top->eval();
+  ASSERT_EQ(top->ALUResult, 0xB43605BB);
+}
+
+TEST_F(ALUTest, SRL) {
+  top->ALUControl = 0b0110;
+  top->SrcA = 0xA020025B;
+  top->SrcB = 8;
+  top->eval();
+  ASSERT_EQ(top->ALUResult, 0x00A02002);
+}
+
+TEST_F(ALUTest, SRA0) {
+  top->ALUControl = 0b0111;
+  top->SrcA = 0x030D37D8;
+  top->SrcB = 5;
+  top->eval();
+  ASSERT_EQ(top->ALUResult, 0x001869BE);
+}
+
+TEST_F(ALUTest, SRA1) {
+  top->ALUControl = 0b0111;
+  top->SrcA = 0x830D37D8;
+  top->SrcB = 5;
+  top->eval();
+  ASSERT_EQ(top->ALUResult, 0xFC1869BE);
+}
+
 TEST_F(ALUTest, OR) {
-  top->ALUControl = 0b011;
+  top->ALUControl = 0b1000;
   top->SrcA = 0x030D37D8;
   top->SrcB = 0xA020025B;
   top->eval();
@@ -82,7 +170,7 @@ TEST_F(ALUTest, OR) {
 }
 
 TEST_F(ALUTest, AND) {
-  top->ALUControl = 0b011;
+  top->ALUControl = 0b1001;
   top->SrcA = 0x29AB0E38;
   top->SrcB = 0xE010F021;
   top->eval();
