ca2d490302a49b55b89b044aa77521842957e1b1
sri-yeah <sriyesh62@gmail.com>
2023-11-30 15:30:37 +0000

    i lied, i fixed it now

--- design/alu/alu.sv
+++ design/alu/alu.sv
@@ -32,14 +32,14 @@ always_comb begin
         4'b0000:  ALUResult = SrcA + SrcB;
         4'b0001:  ALUResult = SrcA - SrcB;
         4'b0010:  ALUResult = (SrcA<<SrcB[SHIFT_WIDTH-1:0]);
-        4'b0011:  case{signs}:
-                    00: ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
-                    01: ALUResult = {DATA_WIDTH-1{1'b0}};
-                    10: ALUResult = {{DATA_WIDTH-1{1'b0}}, {1'b1}};
-                    11: ALUResult = (SrcA[DATA_WIDTH-2:0] < SrcB[DATA_WIDTH-2:0]) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH-1{1'b0}};
-                    default: ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH-1{1'b0}};
+        4'b0011:  case(signs):
+                    2'b00: ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
+                    2'b01: ALUResult = {DATA_WIDTH{1'b0}};
+                    2'b10: ALUResult = {{DATA_WIDTH-1{1'b0}}, {1'b1}};
+                    2'b11: ALUResult = (SrcA[DATA_WIDTH-2:0] < SrcB[DATA_WIDTH-2:0]) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
+                    default: ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
                 endcase
-        4'b0100:  ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH-1{1'b0}};
+        4'b0100:  ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
         4'b0101:  ALUResult = SrcA ^ SrcB;
         4'b0110:  ALUResult = (SrcA>>SrcB[SHIFT_WIDTH-1:0]);
         4'b0111:  ALUResult = (SrcA>>>SrcB[SHIFT_WIDTH-1:0]);
@@ -50,7 +50,7 @@ always_comb begin
         4'b1100:  ALUResult = PC + 4;
         default:  ALUResult = SrcA + SrcB;
     endcase
-    Zero = ({DATA_WIDTH-1{1'b0}} == ALUResult) ? 1'b1 : 1'b0;
+    Zero = ({DATA_WIDTH{1'b0}} == ALUResult) ? 1'b1 : 1'b0;
 end
 
 endmodule
