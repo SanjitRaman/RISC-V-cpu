2acdf6316dfb7f66cbe6e6638a1f23a025902fcb
as9322 <147554866+as9322@users.noreply.github.com>
2023-11-30 19:58:13 +0000

    Update alu.sv  - working SRA without $signed

--- design/alu/alu.sv
+++ design/alu/alu.sv
@@ -10,6 +10,9 @@ module alu #(
     output logic [DATA_WIDTH-1:0]     ALUResult,
     output logic                      Zero
 );
+    
+logic [1:0] signs;
+assign signs = {SrcA[DATA_WIDTH-1], SrcB[DATA_WIDTH-1]};
 
 // 0000 - add
 // 0001 - subtract
@@ -30,11 +33,17 @@ always_comb begin
         4'b0000:  ALUResult = SrcA + SrcB;
         4'b0001:  ALUResult = SrcA - SrcB;
         4'b0010:  ALUResult = (SrcA<<SrcB[SHIFT_WIDTH-1:0]);
-        4'b0011:  ALUResult = ($signed(SrcA) < $signed(SrcB)) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
+        4'b0011:  case(signs)
+                    2'b00: ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
+                    2'b01: ALUResult = {DATA_WIDTH{1'b0}};
+                    2'b10: ALUResult = {{DATA_WIDTH-1{1'b0}}, {1'b1}};
+                    2'b11: ALUResult = (SrcA[DATA_WIDTH-2:0] < SrcB[DATA_WIDTH-2:0]) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
+                    default: ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
+                endcase
         4'b0100:  ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
         4'b0101:  ALUResult = SrcA ^ SrcB;
         4'b0110:  ALUResult = (SrcA>>SrcB[SHIFT_WIDTH-1:0]);
-        4'b0111:  ALUResult = ($signed(SrcA)>>>SrcB[SHIFT_WIDTH-1:0]);
+        4'b0111:  ALUResult = SrcA[31] ? ~({32{1'b1}}>>SrcB[SHIFT_WIDTH-1:0])| SrcA>>SrcB[SHIFT_WIDTH-1:0]: SrcA>>SrcB[SHIFT_WIDTH-1:0];
         4'b1000:  ALUResult = SrcA | SrcB;
         4'b1001:  ALUResult = SrcA & SrcB;
         4'b1010:  ALUResult = (SrcB<<12) + PC;
