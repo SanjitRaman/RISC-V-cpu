8fbcdf96ed05586535517a13d870e2255a864737
sri-yeah <sriyesh62@gmail.com>
2023-11-30 16:05:23 +0000

    test signals removed
    woohoo yippee

--- design/alu/alu.sv
+++ design/alu/alu.sv
@@ -8,8 +8,7 @@ module alu #(
     input  logic [ALU_CTRL_WIDTH-1:0] ALUControl,
     input  logic [DATA_WIDTH-1:0]     PC,
     output logic [DATA_WIDTH-1:0]     ALUResult,
-    output logic                      Zero,
-    output logic [SHIFT_WIDTH-1:0] shiftstuff
+    output logic                      Zero
 );
 
 // 0000 - add
@@ -26,22 +25,11 @@ module alu #(
 // 1011 - load upper
 // 1100 - jal (store pc+4 to register)
 
-logic signs;
-assign signs = {SrcA[DATA_WIDTH-1], SrcB[DATA_WIDTH-1]};
-assign shiftstuff = SrcB[SHIFT_WIDTH-1:0];
-
 always_comb begin
     case(ALUControl)
         4'b0000:  ALUResult = SrcA + SrcB;
         4'b0001:  ALUResult = SrcA - SrcB;
         4'b0010:  ALUResult = (SrcA<<SrcB[SHIFT_WIDTH-1:0]);
-        /* 4'b0011:  case(signs):
-                    2'b00: ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
-                    2'b01: ALUResult = {DATA_WIDTH{1'b0}};
-                    2'b10: ALUResult = {{DATA_WIDTH-1{1'b0}}, {1'b1}};
-                    2'b11: ALUResult = (SrcA[DATA_WIDTH-2:0] < SrcB[DATA_WIDTH-2:0]) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
-                    default: ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
-                endcase */
         4'b0011:  ALUResult = ($signed(SrcA) < $signed(SrcB)) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
         4'b0100:  ALUResult = (SrcA < SrcB) ? {{DATA_WIDTH-1{1'b0}}, {1'b1}} : {DATA_WIDTH{1'b0}};
         4'b0101:  ALUResult = SrcA ^ SrcB;
