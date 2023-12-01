059ba4ec53fda557560e1a73514f0a9d787a213c
as9322 <147554866+as9322@users.noreply.github.com>
2023-12-01 00:54:32 +0000

    Update store.sv

--- design/store/store.sv
+++ design/store/store.sv
@@ -12,7 +12,9 @@ module data_mem #(
 );
 
 logic [1:0] bytepos;
+logic       halfpos;
 assign bytepos = A % 4;
+assign halfpos = A % 2;
 
 always_comb
     case (funct3)
@@ -56,10 +58,18 @@ always_comb
         end
       
         3'b001: begin //store half (sh)
-            WD[BYTE_WIDTH-1:0] = rs2[BYTE_WIDTH-1:0];
-            WD[2*BYTE_WIDTH-1:BYTE_WIDTH] <= rs2[2*BYTE_WIDTH-1:BYTE_WIDTH];
-            WD[3*BYTE_WIDTH-1:2*BYTE_WIDTH] <= RD[3*BYTE_WIDTH-1:2*BYTE_WIDTH];
-            WD[4*BYTE_WIDTH-1:3*BYTE_WIDTH] <= RD[4*BYTE_WIDTH-1:3*BYTE_WIDTH];
+            if(halfpos) begin
+                WD[BYTE_WIDTH-1:0] = RD[BYTE_WIDTH-1:0];
+                WD[2*BYTE_WIDTH-1:BYTE_WIDTH] <= RD[2*BYTE_WIDTH-1:BYTE_WIDTH];
+                WD[3*BYTE_WIDTH-1:2*BYTE_WIDTH] <= rs2[3*BYTE_WIDTH-1:2*BYTE_WIDTH];
+                WD[4*BYTE_WIDTH-1:3*BYTE_WIDTH] <= rs2[4*BYTE_WIDTH-1:3*BYTE_WIDTH];
+            end
+            else begin
+                WD[BYTE_WIDTH-1:0] = RD[BYTE_WIDTH-1:0];
+                WD[2*BYTE_WIDTH-1:BYTE_WIDTH] <= RD[2*BYTE_WIDTH-1:BYTE_WIDTH];
+                WD[3*BYTE_WIDTH-1:2*BYTE_WIDTH] <= rs2[3*BYTE_WIDTH-1:2*BYTE_WIDTH];
+                WD[4*BYTE_WIDTH-1:3*BYTE_WIDTH] <= rs2[4*BYTE_WIDTH-1:3*BYTE_WIDTH];
+            end
         end
       
         3'b010: begin //store word (sw)
