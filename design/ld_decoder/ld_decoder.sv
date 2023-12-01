9762e72b3d9fe7e98ceed6a08c26516a5feda7ca
SanjitRaman <51057192+SanjitRaman@users.noreply.github.com>
2023-12-01 18:23:43 +0530

    refactored ld_decoder.sv

--- design/ld_decoder/ld_decoder.sv
+++ design/ld_decoder/ld_decoder.sv
@@ -3,56 +3,31 @@ module ld_decoder #(
     parameter BYTE_WIDTH = 8,
     parameter ADDRESS_WIDTH = 9
 
-)(
-    input  logic [DATA_WIDTH-1:0]        RD,
-    input  logic [2:0]               funct3,
-    output logic [DATA_WIDTH-1:0]      RDout
+) (
+    input  logic [DATA_WIDTH-1:0] RD,
+    input  logic [           2:0] funct3,
+    output logic [DATA_WIDTH-1:0] RDOut
 );
 
-always_comb begin
+  always_comb
     case (funct3)
-        3'b000: begin // Load byte (lb)
-            RDout[BYTE_WIDTH-1:0] = RD[BYTE_WIDTH-1:0];
-            RDout[2*BYTE_WIDTH-1:BYTE_WIDTH] = {8{RD[7]}};
-            RDout[3*BYTE_WIDTH-1:2*BYTE_WIDTH] = {8{RD[7]}};
-            RDout[4*BYTE_WIDTH-1:3*BYTE_WIDTH] = {8{RD[7]}};
-        end
-                      
-        3'b001: begin // Load Half (lh)
-            RDout[BYTE_WIDTH-1:0] = RD[BYTE_WIDTH-1:0];
-            RDout[2*BYTE_WIDTH-1:BYTE_WIDTH] = RD[2*BYTE_WIDTH-1:BYTE_WIDTH];
-            RDout[3*BYTE_WIDTH-1:2*BYTE_WIDTH] = {8{RD[15]}};
-            RDout[4*BYTE_WIDTH-1:3*BYTE_WIDTH] = {8{RD[15]}};   
-        end       
+      // Load byte (lb)
+      3'b000: RDOut = {{(DATA_WIDTH - 7) {RD[7]}}, RD[6:0]};
 
-        3'b010: begin // Load word (lw) 
-            RDout[BYTE_WIDTH-1:0] = RD[BYTE_WIDTH-1:0];
-            RDout[2*BYTE_WIDTH-1:BYTE_WIDTH] = RD[2*BYTE_WIDTH-1:BYTE_WIDTH];
-            RDout[3*BYTE_WIDTH-1:2*BYTE_WIDTH] = RD[3*BYTE_WIDTH-1:2*BYTE_WIDTH];
-            RDout[4*BYTE_WIDTH-1:3*BYTE_WIDTH] = RD[4*BYTE_WIDTH-1:3*BYTE_WIDTH];
-        end
+      // Load Half (lh)         
+      3'b001: RDOut = {{(DATA_WIDTH - 15) {RD[15]}}, RD[14:0]};
 
-        3'b100: begin// Load byte unsigned (lbu)
-            RDout[BYTE_WIDTH-1:0] = RD[BYTE_WIDTH-1:0];
-            RDout[2*BYTE_WIDTH-1:BYTE_WIDTH] = {8{1'b0}};
-            RDout[3*BYTE_WIDTH-1:2*BYTE_WIDTH] = {8{1'b0}};
-            RDout[4*BYTE_WIDTH-1:3*BYTE_WIDTH] = {8{1'b0}};
-        end
-                
-        3'b101: begin // Load half unsigned (lhu)
-            RDout[BYTE_WIDTH-1:0] = RD[BYTE_WIDTH-1:0];
-            RDout[2*BYTE_WIDTH-1:BYTE_WIDTH] = RD[2*BYTE_WIDTH-1:BYTE_WIDTH];
-            RDout[3*BYTE_WIDTH-1:2*BYTE_WIDTH] = {8{1'b0}};
-            RDout[4*BYTE_WIDTH-1:3*BYTE_WIDTH] = {8{1'b0}};   
-        end
+      // Load word (lw)
+      3'b010: RDOut = RD;
 
-        default: begin
-            RDout[BYTE_WIDTH-1:0] = {8{1'b0}};
-            RDout[2*BYTE_WIDTH-1:BYTE_WIDTH] = {8{1'b0}};
-            RDout[3*BYTE_WIDTH-1:2*BYTE_WIDTH] = {8{1'b0}};
-            RDout[4*BYTE_WIDTH-1:3*BYTE_WIDTH] = {8{1'b0}};
-            end
+      // Load byte unsigned (lbu)
+      3'b100: RDOut = {{(DATA_WIDTH - 7) {1'b0}}, RD[6:0]};
+
+      // Load half unsigned (lhu)
+      3'b101: RDOut = {{(DATA_WIDTH - 15) {1'b0}}, RD[14:0]};
+
+      // default: don't write to show that something is wrong
+      default: RDOut = {DATA_WIDTH{1'b0}};
     endcase
-end
 
 endmodule
