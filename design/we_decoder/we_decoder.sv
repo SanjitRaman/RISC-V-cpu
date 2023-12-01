43f23ee373bfa8355aee248f05cc19568d837694
as9322 <147554866+as9322@users.noreply.github.com>
2023-12-01 19:22:17 +0000

    Update we_decoder.sv

--- design/we_decoder/we_decoder.sv
+++ design/we_decoder/we_decoder.sv
@@ -1,25 +1,30 @@
 module we_decoder #()
 (
     input  logic [2:0]               funct3,
+    input  logic                   MemWrite, 
     output logic                        WE0,
     output logic                        WE1,
     output logic                        WE2,
-    output logic                        WE3,
+    output logic                        WE3
 );
 
 always_comb
-    case (funct3[1:0])
-        //store byte (sb)
-        2'b00:  {WE3, WE2, WE1, WE0} = {0 , 0 , 0 , 1};
+    if (MemWrite) begin
+        case (funct3[1:0])
+            //store byte (sb)
+            2'b00:  {WE3, WE2, WE1, WE0} = {0 , 0 , 0 , 1};
 
-        //store half (sh)
-        2'b01: {WE3, WE2, WE1, WE0} = {0 , 0 , 1 , 1}; 
+            //store half (sh)
+            2'b01: {WE3, WE2, WE1, WE0} = {0 , 0 , 1 , 1}; 
 
-        //store word (sw)
-        2'b10:  {WE3, WE2, WE1, WE0} = {1 , 1 , 1 , 1};
+            //store word (sw)
+            2'b10:  {WE3, WE2, WE1, WE0} = {1 , 1 , 1 , 1};
 
-        // default: don't write to show that something is wrong
-        // in the instruction
-        default: {WE3, WE2, WE1, WE0} = {0 , 0 , 0 , 0};
-    endcase
+            // default: don't write to show that something is wrong
+            // in the instruction
+            default: {WE3, WE2, WE1, WE0} = {0 , 0 , 0 , 0};
+        endcase
+    end
+    else
+        {WE3, WE2, WE1, WE0} = {0 , 0 , 0 , 1};
 endmodule
