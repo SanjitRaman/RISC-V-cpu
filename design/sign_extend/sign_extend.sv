a9d9cc10048ac68f96aaca46adfa458081a5a6bb
SanjitRaman <51057192+SanjitRaman@users.noreply.github.com>
2023-12-03 16:19:01 +0000

    33 bit width reduced to 32 bit width

--- design/sign_extend/sign_extend.sv
+++ design/sign_extend/sign_extend.sv
@@ -14,7 +14,7 @@ module sign_extend #(
             3'b001:   ImmOp = {{20{Instr[31]}}, Instr[31:25], Instr[11:7]};
             3'b010:   ImmOp = {{20{Instr[31]}}, Instr[7], Instr[30:25], Instr[11:8], 1'b0};
             3'b011:   ImmOp = {Instr[31:12], 12'b0};
-            3'b100:   ImmOp = {12'b0, Instr[31], Instr[19:12], Instr[20], Instr[30:21], 1'b0};
+            3'b100:   ImmOp = {11'b0, Instr[31], Instr[19:12], Instr[20], Instr[30:21], 1'b0};
             default:  ImmOp = {{20{Instr[31]}}, Instr[31:20]};
         endcase
     
