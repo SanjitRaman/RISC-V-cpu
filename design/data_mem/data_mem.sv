d2a69038a15736fe80f2d41ba23c83685909c808
SanjitRaman <51057192+SanjitRaman@users.noreply.github.com>
2023-12-04 14:28:16 +0000

    sb indexing reversed

--- design/data_mem/data_mem.sv
+++ design/data_mem/data_mem.sv
@@ -23,16 +23,16 @@ end;
 // TODO: check if writing in between words is allowed.
 always_ff @(posedge CLK) begin
     if (WE0 == 1'b1)
-        ram_array[A] <= WD[31:24];
+        ram_array[A] <= WD[7:0];
     
     if (WE1 == 1'b1)
-        ram_array[A+1] <= WD[23:16];
+        ram_array[A+1] <= WD[15:8];
     
     if (WE2 == 1'b1)
-        ram_array[A+2] <= WD[15:8];
+        ram_array[A+2] <= WD[23:16];
     
     if (WE3 == 1'b1)
-        ram_array[A+3] <= WD[7:0];
+        ram_array[A+3] <= WD[31:24];
 end
     
 assign RD = {ram_array[A+3], ram_array[A+2], ram_array[A+1], ram_array[A]};
