54c527a22178ea89be9da7c54cf2895c19e24801
SanjitRaman <51057192+SanjitRaman@users.noreply.github.com>
2023-12-10 12:36:43 +0000

    All Programs Working Now.

--- testbench/risc_v/versions/risc_v_full_program_tb.h
+++ testbench/risc_v/versions/risc_v_full_program_tb.h
@@ -3,54 +3,91 @@
 #include "risc_v.h"
 #include "vbuddy.cpp"
 #include <string>
-#define MAX_SIM_CYC 5000
-#define PROGRAM_NAME "f1_starting_light"
+#include <iostream>
+#define MAX_SIM_CYC 1'000'000
+#define PROGRAM_NAME "counter"
+#define DATASET "Triangle"
+#define VBUDDY true
 
 int main(int argc, char **argv, char **env) {
-  int simcyc;     // simulation clock count
-  int tick;       // each CLK cycle has two ticks for two edges
+    int simcyc;     // simulation clock count
+    int tick;       // each CLK cycle has two ticks for two edges
 
-  Verilated::commandArgs(argc, argv);
-  std::string command = std::string("make -C .. / assemble PROGRAM_NAME=")+std::string(PROGRAM_NAME);
-  const char* commandArray = command.c_str(); // Convert command string to char array
-  int system_return = system(commandArray); //Bash command
-  // init top verilog instance
-  risc_v * top = new risc_v;
-  // init trace dump
-  Verilated::traceEverOn(true);
-  VerilatedVcdC* tfp = new VerilatedVcdC;
-  top->trace (tfp, 99);
-  tfp->open ("risc_v.vcd");
-  top->address_to_view = 10;
+    Verilated::commandArgs(argc, argv);
+    std::string command = std::string("make -C .. / assemble PROGRAM_NAME=")+std::string(PROGRAM_NAME);
+    const char* commandArray = command.c_str(); // Convert command string to char array
+    int system_return = system(commandArray); //Bash command
+    // init top verilog instance
+    risc_v * top = new risc_v;
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("risc_v.vcd");
+    top->address_to_view = 10;
 
-  if(vbdOpen() != 1) return -1;
-  
-  vbdHeader(PROGRAM_NAME);
- 
-  // initialize simulation inputs
-  top->CLK = 1;
-  top->RST = 1;
-  
-  // run simulation for MAX_SIM_CYC clock cycles
-  for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
-    if(simcyc > 0) top->RST = 0;
-    // dump variables into VCD file and toggle clock
-    for (tick=0; tick<2; tick++) {
-      tfp->dump (2*simcyc+tick);
-      top->CLK = !top->CLK;
-      top->eval();
+    if(VBUDDY) {
+        if(vbdOpen() != 1) return -1;
+        if(PROGRAM_NAME == "pdf") {
+            std::string header = std::string(PROGRAM_NAME) + std::string(": ") + std::string(DATASET);
+            const char* headerArray = header.c_str();
+            vbdHeader(headerArray);
+        }
+        else {
+            vbdHeader(PROGRAM_NAME);
+        }
     }
-    if (Verilated::gotFinish() || vbdGetkey()=='q') break;
+
+
+    // initialize simulation inputs
+    top->CLK = 1;
+    top->RST = 1;
+
+    bool started = false;
+
+    // run simulation for MAX_SIM_CYC clock cycles
+    for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
+        if(simcyc > 0) top->RST = 0;
+        // dump variables into VCD file and toggle clock
+        for (tick=0; tick<2; tick++) {
+            tfp->dump (2*simcyc+tick);
+            top->CLK = !top->CLK;
+            top->eval();
+        }
+        if (Verilated::gotFinish() || vbdGetkey()=='q') break;
+        
+        if(VBUDDY) {
+            if(PROGRAM_NAME == "f1_starting_light") {
+                vbdBar(int(top->reg_output));
+                vbdCycle(simcyc);
+            }
+            else if (PROGRAM_NAME == "pdf") {
+                if(!started && top->pc_viewer == 0xBFC00058)    {
+                    started = true;
+                }
+                else if (started) {
+                    if(simcyc % 4 == 0) {
+                        vbdPlot(top->reg_output, 0, 255);
+                        vbdCycle(simcyc);
+                    }
+                }
+            }
+            else if (PROGRAM_NAME == "sinegen") {
+                if(simcyc % 4 == 0) {
+                    vbdPlot(top->reg_output, 0, 255);
+                    vbdCycle(simcyc);
+                }
+            }
+            else {
+                vbdPlot(top->reg_output, 0, 255);
+                vbdCycle(simcyc);
+            }
+            
+        }
     
-    if(PROGRAM_NAME == "f1_starting_light")
-      vbdBar(int(top->reg_output));
-    else
-      vbdPlot(top->reg_output, 0, 255);
-    
-    
-    vbdCycle(simcyc);
-  }
-  vbdClose();
-  tfp->close(); 
-  exit(0);
+    }
+
+    if(VBUDDY) vbdClose();
+    tfp->close(); 
+    exit(0);
 }
