009e345a0e2be7ef0dd73a8a361af3216c30e8e2
sri-yeah <sriyesh62@gmail.com>
2023-12-11 00:29:22 +0000

    simple cache unit implemented
    surrounding muxes need to be implemented in top level for any practical use. needs to be modified later for N-way set associative and for write back policy.

--- design/cache/cache.sv
+++ design/cache/cache.sv
@@ -1,8 +1,12 @@
 module cache #(
+    parameter DATA_WIDTH = 32,
+    parameter TAG_WIDTH = 27,
+    parameter SET_ADDRESS_WIDTH = 3, 
+    parameter BYTE_WIDTH = 8
 
 ) (
     input logic CLK,
-    input logic RST, // TODO: implement reset set valid bit to 0 for everything.
+    input logic RST, 
     input logic WE0,
     input logic WE1,
     input logic WE2,
@@ -21,8 +25,51 @@ module cache #(
     output logic [DATA_WIDTH-1:0] WDCache,
     output logic [DATA_WIDTH-1:0] RD,
 
-
-
 );
+
+    int CACHE_WIDTH = (1 + TAG_WIDTH + DATA_WIDTH);
+
+    logic [CACHE_WIDTH-1:0] cache_array [2**SET_ADDRESS_WIDTH-1:0];
+    logic [SET_ADDRESS_WIDTH-1:0] set;
+    logic V;
+    logic [TAG_WIDTH-1:0] tag;
+    logic [CACHE_WIDTH-1:0] CIN;
+
+assign tag = A[DATA_WIDTH-1:5]
+assign set = A[4:2];
+
+always_ff @(posedge RST or negedge CLK) begin
+    if(RST) begin
+        hit <= 0;
+        WE0Cache <= 0;
+        WE1Cache <= 0;
+        WE2Cache <= 0;
+        WE3Cache <= 0;
+        ACache <= 0;
+        WDCache <= 0;
+        RD <= 0;
+    end
+    else begin
+        V = cache_array[set][CACHE_WIDTH-1]
+        if(~V) begin
+            A[set] <= {1'b1, tag, ({BYTE_WIDTH{WE3}} & WD[31:24]), ({BYTE_WIDTH{WE2}} & WD[23:16]), ({BYTE_WIDTH{WE1}} & WD[15:8]), ({BYTE_WIDTH{WE0}} & WD[7:0])};
+            WE0Cache <= 0;
+            WE1Cache <= 0;
+            WE2Cache <= 0;
+            WE3Cache <= 0;
+        end
+        else begin
+            WDCache <= WD;
+            WE0Cache <= WE0;
+            WE1Cache <= WE1;
+            WE2Cache <= WE2;
+            WE3Cache <= WE3;
+        end
+    end
+
+end
+
+assign RD = cache_array[set][DATA_WIDTH-1:0];
+assign hit = (cache_array[set][CACHE_WIDTH-1] && (tag == cache_array[set][TAG_WIDTH+DATA_WIDTH-1:DATA_WIDTH]));
 
 endmodule
