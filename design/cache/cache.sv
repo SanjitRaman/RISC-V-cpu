c87e8061c78d0b565dc57512e2e9aa54e7ed4db3
sri-yeah <sriyesh62@gmail.com>
2023-12-12 15:52:18 +0000

    cache.sv fully implemented
    ready to be taken over to pipeline stage, only requires changes in top level

--- design/cache/cache.sv
+++ design/cache/cache.sv
@@ -11,12 +11,12 @@ module cache #(
     input logic WE1,
     input logic WE2,
     input logic WE3,
-    
+    input logic MemRead,
+    input logic MemWrite, 
     input logic [DATA_WIDTH-1:0] A,
     input logic [DATA_WIDTH-1:0] WD,
     
     output logic hit,
-
     output logic WE0Cache,
     output logic WE1Cache,
     output logic WE2Cache,
@@ -27,49 +27,66 @@ module cache #(
 
 );
 
-    int CACHE_WIDTH = (1 + TAG_WIDTH + DATA_WIDTH);
+    int CACHE_WIDTH = (2 + TAG_WIDTH + DATA_WIDTH);
 
     logic [CACHE_WIDTH-1:0] cache_array [2**SET_ADDRESS_WIDTH-1:0];
     logic [SET_ADDRESS_WIDTH-1:0] set;
     logic V;
+    logic D;
     logic [TAG_WIDTH-1:0] tag;
+    logic [TAG_WIDTH-1:0] tagc;
     logic [CACHE_WIDTH-1:0] CIN;
 
 assign tag = A[DATA_WIDTH-1:5]
-assign set = A[4:2];
+assign set = A[4:2]; // 8 sets (3 bits)
 
-always_ff @(posedge RST or negedge CLK) begin
-    if(RST) begin
-        hit <= 0;
-        WE0Cache <= 0;
-        WE1Cache <= 0;
-        WE2Cache <= 0;
-        WE3Cache <= 0;
-        ACache <= 0;
-        WDCache <= 0;
-        RD <= 0;
+
+always_ff @(posedge RST) begin
+    hit <= 0;
+    WE0Cache <= 0;
+    WE1Cache <= 0;
+    WE2Cache <= 0;
+    WE3Cache <= 0;
+    ACache <= 0;
+    WDCache <= 0;
+    RD <= 0;
+end
+
+always_ff @(posedge CLK) begin
+    tagc = cache_array[set][DATA_WIDTH+TAG_WIDTH-1:DATA_WIDTH];
+
+    if(MemRead) begin
+        RD <= cache_array[set][DATA_WIDTH-1:0];
+        hit <= (cache_array[set][CACHE_WIDTH-1] && (tag == tagc));
     end
-    else begin
-        V = cache_array[set][CACHE_WIDTH-1]
+    
+     if(MemWrite) begin
+        V = cache_array[set][CACHE_WIDTH-2]
+        D = cache_array[set][CACHE_WIDTH-1]
         if(~V) begin
-            A[set] <= {1'b1, tag, ({BYTE_WIDTH{WE3}} & WD[31:24]), ({BYTE_WIDTH{WE2}} & WD[23:16]), ({BYTE_WIDTH{WE1}} & WD[15:8]), ({BYTE_WIDTH{WE0}} & WD[7:0])};
+            // Cache - {Dirty, Valid, Tag, Write data}
+            cache_array[set] <= {2'b11, tag, ({BYTE_WIDTH{WE3}} & WD[31:24]), ({BYTE_WIDTH{WE2}} & WD[23:16]), ({BYTE_WIDTH{WE1}} & WD[15:8]), ({BYTE_WIDTH{WE0}} & WD[7:0])};
             WE0Cache <= 0;
             WE1Cache <= 0;
             WE2Cache <= 0;
             WE3Cache <= 0;
         end
         else begin
-            WDCache <= WD;
-            WE0Cache <= WE0;
-            WE1Cache <= WE1;
-            WE2Cache <= WE2;
-            WE3Cache <= WE3;
+            WDCache <= cache_array[set][DATA_WIDTH-1:0];
+            ACache  <= {cache_array[set][tagc], set, 2'b00};
+            WE0Cache <= 1;
+            WE1Cache <= 1;
+            WE2Cache <= 1;
+            WE3Cache <= 1;
+            cache_array[set] <= {2'b11, tag, ({BYTE_WIDTH{WE3}} & WD[31:24]), ({BYTE_WIDTH{WE2}} & WD[23:16]), ({BYTE_WIDTH{WE1}} & WD[15:8]), ({BYTE_WIDTH{WE0}} & WD[7:0])};
         end
     end
-
 end
 
-assign RD = cache_array[set][DATA_WIDTH-1:0];
-assign hit = (cache_array[set][CACHE_WIDTH-1] && (tag == cache_array[set][TAG_WIDTH+DATA_WIDTH-1:DATA_WIDTH]));
+always_ff @(negedge CLK) begin
+    if(MemRead) begin
+        A[set] <= {2'b01, tag, ({BYTE_WIDTH{WE3}} & WD[31:24]), ({BYTE_WIDTH{WE2}} & WD[23:16]), ({BYTE_WIDTH{WE1}} & WD[15:8]), ({BYTE_WIDTH{WE0}} & WD[7:0])};
+    end
+end
 
 endmodule
