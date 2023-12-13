88e76312bd8f84e453c682b61afc08a5ce9992c3
as9322 <147554866+as9322@users.noreply.github.com>
2023-12-13 12:25:47 +0000

    Update cache.sv - fixed multidriven errors

--- design/cache/cache.sv
+++ design/cache/cache.sv
@@ -41,78 +41,75 @@ module cache #(
     logic hit0;
     logic hit1;
 
-
-always_ff @(posedge RST) begin
-    hit <= 0;
-    WECache <= 0;
-    ACache <= 0;
-    WDCache <= 0;
-    RD <= 0;
-end
-
-always_ff @(posedge CLK) begin
-    tag0 = cache_array[set][CACHE_WIDTH-3:CACHE_WIDTH-3-(TAG_WIDTH-1)];
-    tag1 = cache_array[set][DATA_WIDTH+TAG_WIDTH-1:DATA_WIDTH];
-    
-    if(MemRead) begin
-        hit0 = (cache_array[set][CACHE_WIDTH-2] && (tag == tag0));
-        hit1 = (cache_array[set][(CACHE_WIDTH/2)-2] && (tag == tag0));
-        RD <= hit1 ? cache_array[set][(CACHE_WIDTH/2)+(DATA_WIDTH-1):CACHE_WIDTH/2] : cache_array[set][DATA_WIDTH-1:0];
-        hit <= hit0 | hit1;
+always_ff @(posedge CLK, posedge RST) begin
+    if(RST) begin
+        hit <= 0;
+        WECache <= 0;
+        ACache <= 0;
+        WDCache <= 0;
+        RD <= 0;
     end
+    else if (CLK) begin
+        tag0 = cache_array[set][CACHE_WIDTH-3:CACHE_WIDTH-3-(TAG_WIDTH-1)];
+        tag1 = cache_array[set][DATA_WIDTH+TAG_WIDTH-1:DATA_WIDTH];
 
-end
-
-always_ff @(negedge CLK) begin
-    tag0 = cache_array[set][CACHE_WIDTH-3:CACHE_WIDTH-3-(TAG_WIDTH-1)];
-    tag1 = cache_array[set][DATA_WIDTH+TAG_WIDTH-1:DATA_WIDTH];
-    D1 = cache_array[set][(CACHE_WIDTH/2)-1];
-
-    if(MemRead & ~hit) begin
-        WDCache <= cache_array[set][DATA_WIDTH-1:0];
-        ACache  <= {cache_array[set][tag1], set, 2'b00};
-        WECache <= D1;
-        cache_array[set] <= ({2'b01, tag, 
-                                ({BYTE_WIDTH{WE3}} & WD[31:24]), 
-                                ({BYTE_WIDTH{WE2}} & WD[23:16]), 
-                                ({BYTE_WIDTH{WE1}} & WD[15:8]), 
-                                ({BYTE_WIDTH{WE0}} & WD[7:0]), 
-                                {(CACHE_WIDTH/2){1'b0}}
-                                }) 
-                                | (cache_array[set]>>(CACHE_WIDTH/WAYS));
-        
-    end
-
-    if(MemWrite) begin
-        V0 = cache_array[set][CACHE_WIDTH-2];
-        if(~V0) begin
-            // Cache - {Dirty, Valid, Tag, Write data}
-            cache_array[set] <= {2'b11, tag, 
-            ({BYTE_WIDTH{WE3}} & WD[31:24]), 
-            ({BYTE_WIDTH{WE2}} & WD[23:16]), 
-            ({BYTE_WIDTH{WE1}} & WD[15:8]), 
-            ({BYTE_WIDTH{WE0}} & WD[7:0]),
-            {(CACHE_WIDTH/2){1'b0}}
-            };
-            WECache <= 0;
+        if(MemRead) begin
+            hit0 = (cache_array[set][CACHE_WIDTH-2] && (tag == tag0));
+            hit1 = (cache_array[set][(CACHE_WIDTH/2)-2] && (tag == tag0));
+            RD <= hit1 ? cache_array[set][(CACHE_WIDTH/2)+(DATA_WIDTH-1):CACHE_WIDTH/2] : cache_array[set][DATA_WIDTH-1:0];
+            hit <= hit0 | hit1;
         end
-        else begin
+    end
+    else begin
+        tag0 = cache_array[set][CACHE_WIDTH-3:CACHE_WIDTH-3-(TAG_WIDTH-1)];
+        tag1 = cache_array[set][DATA_WIDTH+TAG_WIDTH-1:DATA_WIDTH];
+        D1 = cache_array[set][(CACHE_WIDTH/2)-1];
+
+        if(MemRead & ~hit) begin
             WDCache <= cache_array[set][DATA_WIDTH-1:0];
             ACache  <= {cache_array[set][tag1], set, 2'b00};
             WECache <= D1;
-
-            cache_array[set] <= ({2'b11, tag, 
-                                ({BYTE_WIDTH{WE3}} & WD[31:24]), 
-                                ({BYTE_WIDTH{WE2}} & WD[23:16]), 
-                                ({BYTE_WIDTH{WE1}} & WD[15:8]), 
-                                ({BYTE_WIDTH{WE0}} & WD[7:0]), 
-                                {(CACHE_WIDTH/2){1'b0}}
-                                }) 
-                                | (cache_array[set]>>(CACHE_WIDTH/WAYS));
+            cache_array[set] <= ({2'b01, tag, 
+                                    ({BYTE_WIDTH{WE3}} & WD[31:24]), 
+                                    ({BYTE_WIDTH{WE2}} & WD[23:16]), 
+                                    ({BYTE_WIDTH{WE1}} & WD[15:8]), 
+                                    ({BYTE_WIDTH{WE0}} & WD[7:0]), 
+                                    {(CACHE_WIDTH/2){1'b0}}
+                                    }) 
+                                    | (cache_array[set]>>(CACHE_WIDTH/WAYS));
 
         end
-    end
 
+        if(MemWrite) begin
+            V0 = cache_array[set][CACHE_WIDTH-2];
+            if(~V0) begin
+                // Cache - {Dirty, Valid, Tag, Write data}
+                cache_array[set] <= {2'b11, tag, 
+                ({BYTE_WIDTH{WE3}} & WD[31:24]), 
+                ({BYTE_WIDTH{WE2}} & WD[23:16]), 
+                ({BYTE_WIDTH{WE1}} & WD[15:8]), 
+                ({BYTE_WIDTH{WE0}} & WD[7:0]),
+                {(CACHE_WIDTH/2){1'b0}}
+                };
+                WECache <= 0;
+            end
+            else begin
+                WDCache <= cache_array[set][DATA_WIDTH-1:0];
+                ACache  <= {cache_array[set][tag1], set, 2'b00};
+                WECache <= D1;
+
+                cache_array[set] <= ({2'b11, tag, 
+                                    ({BYTE_WIDTH{WE3}} & WD[31:24]), 
+                                    ({BYTE_WIDTH{WE2}} & WD[23:16]), 
+                                    ({BYTE_WIDTH{WE1}} & WD[15:8]), 
+                                    ({BYTE_WIDTH{WE0}} & WD[7:0]), 
+                                    {(CACHE_WIDTH/2){1'b0}}
+                                    }) 
+                                    | (cache_array[set]>>(CACHE_WIDTH/WAYS));
+
+            end
+        end
+    end 
 end
 
 endmodule
